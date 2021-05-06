40181d2409fe33769d18317547d208b2ff3f16a7
wangjx <wangjx@code.com>
2021-05-06 20:49:40 +0800

    first commit

---
+++ src/main/java/com/wangjx/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.wangjx.git;
+
+/**
+ * @author wjx
+ * @date 2021/5/6 - 20:38
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
